93fc22d41a47949fd31ad9641ada11fb5ffd4c4e
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-05-29 18:02:55 -0700

    update : to_tsv generator updated.

--- out/sample-advice-generation/to_tsv.py
+++ out/sample-advice-generation/to_tsv.py
@@ -5,13 +5,15 @@ import json
 jsonl = sys.argv[1]
 df_dict = {
     "statement": [],
-    "hints": [],
-    "advice[IR]": [],
-    "advice[RH]": [],
-    "advice[EXP]": [],
-    "advice[SQ]": [],
-    "advice[CA]": [],
-    "advice[CP]": []
+    "gold": [],
+    "label": [],
+    "IR": [],
+    "RiskHighlighting": [],
+    "Explanatory": [],
+    "Socratic Questioning (Before Advice)": [],
+    "Socratic Questioning (After Advice)": [],
+    "Stating Alternatives": [],
+    "Counterfactual Prompting": []
 }
 
 with open(jsonl, 'r') as f:
@@ -20,13 +22,17 @@ with open(jsonl, 'r') as f:
         hints = ' '.join([f"{hint['text']}" for hint in data.get('gold_evidence', [])])
         
         df_dict["statement"].append(data.get('text', ''))
-        df_dict["hints"].append(hints)
-        df_dict["advice[IR]"].append(data.get('advice', {}).get('informationretrieval', '').replace('+', '[+]'))
-        df_dict["advice[RH]"].append(data.get('advice', {}).get('riskhighlighting', ''))
-        df_dict["advice[EXP]"].append(data.get('advice', {}).get('explanatory', ''))
-        df_dict["advice[SQ]"].append(data.get('advice', {}).get('socraticquestioning', ''))
-        df_dict["advice[CA]"].append(data.get('advice', {}).get('comparealternatives', ''))
-        df_dict["advice[CP]"].append(data.get('advice', {}).get('counterfactualprompt', ''))
+        df_dict["gold"].append(hints)
+        df_dict["label"].append(data.get('label', ''))
+        advice = data.get('advice', {})
+        df_dict["IR"].append(advice.get('informationretrieval', ''))
+        df_dict["RiskHighlighting"].append(advice.get('riskhighlighting', ''))
+        df_dict["Explanatory"].append(advice.get('explanatory', ''))
+        df_dict["Socratic Questioning (Before Advice)"].append(advice.get('socraticquestioningbeforeevidence', ''))
+        df_dict["Socratic Questioning (After Advice)"].append(advice.get('socraticquestioningafterevidence', ''))
+        df_dict["Stating Alternatives"].append(advice.get('statealternatives', ''))
+        df_dict["Counterfactual Prompting"].append(advice.get('counterfactualprompt', ''))
+
 
 df = pd.DataFrame(df_dict)
 df.to_csv(jsonl.replace('.jsonl', '.tsv'), sep='\t', index=False, header=True)
